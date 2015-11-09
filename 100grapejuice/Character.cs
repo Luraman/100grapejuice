875fe1855e62ac99eb51ea71ef9decb2bff33869
Luraman <ponifi3d@gmail.com>
2015-11-09 20:09:55 +0100

    Character and Bar

---
+++ 100grapejuice/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100grapejuice
+{
+  class Character
+  {
+    public Character(int maxHealth)
+    {
+      Health = new Bar(maxHealth, true);
+      Health.Depleted += () => Dead();
+    }
+
+    public Bar Health { get; private set; }
+
+    public event Action Dead;
+
+    public void Damage(int amount, out int walled)
+    {
+      {
+        Health.Change(-amount, out walled);
+      }
+    }
+  }
+}
---
+++ 100grapejuice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100grapejuice
+{
+  class Program
+  {
+    public static void CallEvent(Action _event)
+    {
+      if (_event != null)
+      {
+        _event();
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      Character Luraman = new Character(5);
+
+      Action DeathNote = () => Console.WriteLine("u r ded");
+
+      Luraman.Dead += DeathNote;
+
+      int _;
+      Luraman.Damage(-2, out _);
+      Luraman.Damage(5, out _);
+
+      Console.ReadKey();
+    }
+  }
+}
